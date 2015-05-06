067a3d4d98ca9bddcefc9105833a6f132c9718d0
RasheedRomain <rj.romain12@gmail.com>
2015-05-06 11:22:02 -0700

    thrid commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
 class PagesController < ApplicationController
+ 	def index
+ 	end
+ 	private 
+ 	
+ 	def user_params
+ 	 	params.require(:user).permit(:email, :user_name links_attributes: [ :url, :title ])
+ 	end 
 end
